6a5d688981139d375ed1539d2df13a39489ccb5c
JMsuper <tjsqls2067@naver.com>
2022-08-10 16:39:36 +0900

    [Feat] 컨트랙 호출 메소드

--- flutter/nft_app/lib/main.dart
+++ flutter/nft_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +33,179 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var totalVotesA;
+  var totalVotesB;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  late Client httpClient;
+  late Web3Client ethClient;
+
+  //Ethereum address
+  final String myAddress = "0x0cBd52136FE6EAcCB3c0534f16974D9DE2Db14E7";
+
+  //url from Infura
+  final String blockchainUrl =
+      "https://rinkeby.infura.io/v3/343f7e7be4d44389a8202a838b26e2c8";
+
+  @override
+  void initState() {
+    httpClient = Client();
+    ethClient = Web3Client(blockchainUrl, httpClient);
+    getTotalVotes();
+    super.initState();
+  }
+
+  Future<DeployedContract> getContract() async {
+    // obtain our smart contract using rootbundle to access our json file
+    String abiFile = await rootBundle.loadString("assets/contract.json");
+    String contractAddress = "0x674757ce53D27d4Baa3fb6E7e010F51d897a1401";
+    final contract = DeployedContract(ContractAbi.fromJson(abiFile, "Voting"),
+        EthereumAddress.fromHex(contractAddress));
+    return contract;
+  }
+
+  Future<List<dynamic>> callFunction(String name) async {
+    final contract = await getContract();
+    final function = contract.function(name);
+    final result = await ethClient
+        .call(contract: contract, function: function, params: []);
+    return result;
+  }
+
+  Future<void> getTotalVotes() async {
+    List<dynamic> resultsA = await callFunction("getTotalVotesAlpha");
+    List<dynamic> resultsB = await callFunction("getTotalVotesBeta");
+    totalVotesA = resultsA[0];
+    totalVotesB = resultsB[0];
+
+    setState(() {});
+  }
+
+  Future<void> vote(bool voteAlpha) async {
+    snackBar(label: "Recording vote");
+    // obtain private key for write operation
+    String privateKey =
+        "e188f06ea30be086ed4e229978bc2915f2b0d4c73dda97babd58cc5ae01c9ae8";
+    Credentials key = EthPrivateKey.fromHex(privateKey);
+    // obtain our contract from abi in json file
+    final contract = await getContract();
+
+    // extract function from json file
+    final function = contract.function(voteAlpha ? "voteAlpha" : "voteBeta");
+
+    // send transaction using the our private key, function and contract
+    await ethClient.sendTransaction(
+        key,
+        Transaction.callContract(
+            contract: contract, function: function, parameters: []),
+        chainId: 4);
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    snackBar(label: "verifying vote");
+    // set a 20 seconds delay to allow the transaction to be verified before trying to retrieve the balance
+    Future.delayed(const Duration(seconds: 20), () {
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
+      snackBar(label: "retrieving votes");
+      getTotalVotes();
+      ScaffoldMessenger.of(context).clearSnackBars();
     });
+  }
+
+  snackBar({String? label}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(label!),
+            CircularProgressIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+        duration: Duration(days: 1),
+        backgroundColor: Colors.blue,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(30),
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          child: Text("A"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Total Votes: ${totalVotesA ?? ""}",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          child: Text("B"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("Total Votes: ${totalVotesB ?? ""}",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      vote(true);
+                    },
+                    child: Text('Vote Alpha'),
+                    style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      vote(false);
+                    },
+                    child: Text('Vote Beta'),
+                    style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
